8f49013c39e546e995cd302d0866863ca9206a72
donk7413 <32650379+donk7413@users.noreply.github.com>
2021-04-27 16:41:35 +0200

    new version 4

--- bin/x64/plugins/cyber_engine_tweaks/mods/000sound_mod/init.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/000sound_mod/init.lua
@@ -48,4 +48,145 @@ function SoundManager.PlaySound(file,path,channel)
 end
 
 
+
+function SoundManager.Pause(channel)
+	
+	
+		
+		if(channel == "env") then
+			io.open("pauseenv.txt","w"):close()
+			
+			
+		end
+		
+		
+		if(channel == "music") then
+			io.open("pausemusic.txt","w"):close()
+			
+		end
+		
+		if(channel == "sound") then
+			io.open("pausesound.txt","w"):close()
+			
+		end
+		
+		
+		
+	
+	
+end
+
+
+function SoundManager.Resume(channel)
+	
+	
+		
+		if(channel == "env") then
+			
+			os.remove("pauseenv.txt")
+			
+		end
+		
+		
+		if(channel == "music") then
+			
+				os.remove("pausemusic.txt")
+		end
+		
+		if(channel == "sound") then
+			
+				os.remove("pausesound.txt")
+		end
+		
+		
+		
+	
+	
+end
+
+
+function SoundManager.Stop(channel)
+	
+	
+		
+		if(channel == "env") then
+			
+			os.remove("pauseenv.txt")
+				io.open("stopenv.txt","w"):close()
+		end
+		
+		
+		if(channel == "music") then
+			
+					os.remove("pausemusic.txt")
+				io.open("stopmusic.txt","w"):close()
+		end
+		
+		if(channel == "sound") then
+			
+				os.remove("pausesound.txt")
+				io.open("stopsound.txt","w"):close()
+		end
+		
+		
+		
+	
+	
+end
+
+
+
+function SoundManager.IsPlaying(channel)
+	
+local bool = false
+	
+		
+		if(channel == "env") then
+			
+			
+			local f = io.open("env.txt")
+			local lines = f:read("*a")
+				
+			if(lines ~= "") then
+			
+				bool = true
+				
+			
+			end
+			
+		end
+		
+		if(channel == "music") then
+			
+			
+			local f = io.open("music.txt")
+			local lines = f:read("*a")
+				
+			if(lines ~= "") then
+			
+				bool = true
+				
+			
+			end
+			
+		end
+		
+		if(channel == "sound") then
+			local f = io.open("sound.txt")
+			local lines = f:read("*a")
+				
+			if(lines ~= "") then
+			
+				bool = true
+				
+			
+			end
+		end
+		
+		return bool
+	
+	
+end
+
+
 return SoundManager
