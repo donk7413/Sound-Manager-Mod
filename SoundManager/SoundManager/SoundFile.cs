364d8385dd2a145a68e6b8b9bfbda6eb89554ef1
ddxover <ddxover@hotmail.fr>
2021-07-16 23:36:10 +0200

    Nugget Clean up

--- SoundManager/SoundManager/SoundFile.cs
+++ SoundManager/SoundManager/SoundFile.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SoundManager
+﻿namespace SoundManager
 {
     public class SoundFile
     {
         public string path { get; set; }
 
         public double volume { get; set; }
-
-
     }
 }
